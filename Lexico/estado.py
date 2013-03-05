342cded4fcb0206bb5143958922229bc814ed2aa
Gabriel Maia Ferraz <gabriel_ferraz@(none)>
2013-03-05 14:51:00 -0300

    Remanejamento dos arquivos

---
+++ Lexico/estado.py
@@ -0,0 +1,83 @@
+def confere(tipo, entrada):
+	if tipo == "minusculo":
+		return "a" <= entrada <= "z"
+	elif tipo == "maiusculo":
+		return "A" <= entrada <= "Z"
+	elif tipo == "digito":
+		return "0" <= entrada <= "9"
+	elif tipo == "naozero":
+		return "1" <= entrada <= "9"
+	elif tipo == "hexamin":
+		return "a" <= entrada <= "f"
+	elif tipo == "hexamai":
+		return "F" <= entrada <= "F"
+	else:
+		return entrada == tipo
+
+class Estado:
+	
+	def __init__(self, nome, final):
+		self.nome = nome
+		self.final = final
+		self.transicoes = {}
+
+	def __str__(self):
+		return self.nome
+		
+	def set_transicoes(self, transicoes):
+		self.transicoes = transicoes
+	
+	def	fim(self):
+		return self.final
+		
+	def get_proximo(self, entrada):
+		for tipo in self.transicoes.keys():
+			if confere(tipo, entrada):
+				#print tipo,"->",str(self.transicoes[tipo])
+				return self.transicoes[tipo]
+		
+		if "outro" in self.transicoes.keys():
+				#print "outro","->",str(self.transicoes["outro"])
+				return self.transicoes["outro"]
+			
+		raise Exception("Proximo nao encontrado")
+
+class Automato:
+	
+	def __init__(self, nome, estados, inicial):
+		self.estados = estados
+		self.inicial = inicial
+		self.atual = inicial
+		self.nome = nome
+
+	def __str__(self):
+		return str(self.atual)
+		
+	def get_nome(self):
+		return self.nome
+	
+	def inicializa(self):
+		self.atual = self.inicial
+
+	def get_atual(self):
+		return self.atual
+		
+	def proximo(self, entrada):
+		self.atual = self.atual.get_proximo(entrada)
+		
+	def fim(self):
+		return self.atual.fim()
+
+
+if __name__ == "__main__":
+	estados = []
+	estados.append(Estado("q0", False))
+	estados.append(Estado("q1", False))
+	estados.append(Estado("q2", True))
+
+	estados[0].set_transicoes({"#": estados[1]})
+	estados[1].set_transicoes({"\n": estados[2],"outro":estados[1]})
+
+	automato = Automato(estados, estados[0])
+	#automatos["comentario"] = Automato(estados, estados[0])
+
---
+++ Top-Down/desc_pred.py
@@ -0,0 +1,36 @@
+from gramatica import m
+entrada = [('tipo', 'int'), ('identificador', 'x'), ('identificador', 'x'), ('logico', '='), ('int', '3'), ('identificador', 'x'), ('logico', '='), ('int', '3'), ('aritmetico', '+'), ('int', '4'),("$","$")]
+for i in range(0,len(entrada)):
+	if entrada[i][0] in ["aritmetico", "logico"]:
+		entrada[i] = entrada[i][1],entrada[i][0]
+proximo = entrada.pop(0)
+pilha = ["P"]
+terminais = ["+=","-=","*=","/=","%=","**=","=", "tipo", "identificador", "<",">","<=",">=","!=","==","*", "//", "/", "+", "-", "&&", "||", "!", "int", "e"]
+nao_terminais = [x for x in m]
+x = pilha[-1]
+while x != "$":
+	if x in terminais:
+		if x == "e":
+			pilha.pop(-1)
+			x = pilha[-1]
+		elif x == proximo[0]:
+			pilha.pop(-1)
+			x = pilha[-1]
+			proximo = entrada.pop(0)
+		else:
+			print "Erro de Sintaxe"
+	elif x in nao_terminais:
+		if pilha[-1] != "P":
+			pilha.pop(-1)
+		aux = m[x][proximo[0]].split(" ")
+		aux.reverse()
+		for i in aux:
+			pilha.append(i)
+		x = pilha[-1]
+	else:
+		print "Erro de Sintaxe"
+if proximo[0] != "$":
+	print "Erro de Sintaxe"
+print "Programa sem erros"
+
+
