5252d61bd7d6babd289bc605b46630263633c4a2
Erik Vinicius Gomes Almeida <erikvinicius5@gmail.com>
2013-04-08 19:46:24 -0300

    num mudei nada

---
+++ Semantico/saida.c
@@ -0,0 +1,39 @@
+if !(1 ) goto 0;
+
+int var1 ;
+var1 = 23 ;
+goto fim36;
+label 0:
+
+int var3 ;
+if !(1 ) goto 54;
+
+int var4 ;
+var3 = 50 ;
+goto fim93;
+label 54:
+
+var3 = 12 ;
+label fim93:
+
+var3 = 20 ;
+label fim36:
+
+
+ int var2 ;
+int ____vish = 1 - var1;
+label 142:
+: ;
+var2 = 10 ;
+__vish++;
+if (__vish != 0) goto 142;
+
+
+ int bli 
+
+ bli = 3 
+
+ char a ;
+int c 
+
+ 
